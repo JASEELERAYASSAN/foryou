fba1584d1c7b42c853ed1b3d7604e079b9208e00
jaseel <jaseelerayassan@gmail.com>
2026-02-15 00:26:07 +0530

    feat: Add GitHub Pages deployment workflow and configure Nuxt for static site generation.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false, // Ensure consistent behavior for static hosting
+  app: {
+    baseURL: '/foryou/', // Matches the repository name
+    buildAssetsDir: 'assets', // Avoids issues with github underscores
+  },
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  nitro: {
+    static: true
+  }
 })
